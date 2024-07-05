63bbf11473ae77e58724c9db6f44be13fb14c272
BO HENG <2904240634@qq.com>
2024-07-05 08:50:26 +0800

    test2

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -20,9 +20,9 @@
 #define layerEditMode layerEditButton.isPressed
 #define modifyLineWidthMode modifyLineWidthButton.isPressed
 
-#define canBeSelected (selectMode || zoomMode || layerEditMode || modifyLineWidthMode)//
+#define canBeSelected (selectMode || zoomMode || layerEditMode || modifyLineWidthMode)
 
-
+//test2
 
 static int COLOR = WHITE;
 int selectedIndex = -1;
